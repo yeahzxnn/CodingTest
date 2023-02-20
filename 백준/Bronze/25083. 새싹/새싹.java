e6ac131063046a34aa65e08ca378025f2e9f972a
SYJ <yejin0621@tukorea.ac.kr>
2023-02-21 00:52:39 +0900

    [Bronze V] Title: 새싹, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.printf("         ,%c\'\"%d\n",'r',7); 
+        System.out.printf("%c`-_   ,\'  ,/\n",'r'); 
+        System.out.printf(" \\. \". %c_%c\'\n",'L','r'); 
+        System.out.printf("   `~\\/\n");
+        System.out.printf("      |\n");
+        System.out.printf("      |\n");
+            
+    }
+}
