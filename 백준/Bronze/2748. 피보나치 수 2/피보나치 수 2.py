3a6aa91337bebe229f087b64bcf33677806b1034
SYJ <yejin0621@tukorea.ac.kr>
2023-04-30 01:33:10 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,14 +1,9 @@
 n = int(input())
 
-fibonacci = []
-num = 0
-for i in range(n+1):
-    if i == 0:
-        num = 0
-    elif i <= 2:
-        num = 1
-    else:
-        num = fibonacci[-1]+fibonacci[-2]
-    fibonacci.append(num)
-    
-print(fibonacci[-1])
+fibo = []
+fibo.append(0)
+fibo.append(1)
+
+for i in range(2, n+1):
+    fibo.append(fibo[i-1]+fibo[i-2])
+print(fibo[n])
