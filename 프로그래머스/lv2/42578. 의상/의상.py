9bbb9ac86d66b89e99b73fd9692fe61cd59c5569
SYJ <yejin0621@tukorea.ac.kr>
2023-06-18 00:55:22 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -0,0 +1,10 @@
+def solution(clothes):
+    hash_map = {}
+    for clothe, type in clothes:
+        hash_map[type] = hash_map.get(type, 0)+1
+    answer = 1
+    for type in hash_map:
+        answer *= (hash_map[type]+1)
+        
+    return answer - 1
+print(solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]))
