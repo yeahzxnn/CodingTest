2c0529c84bc1c1d45e1638d091406f4bd1921b9b
YEJIN SHIN (KAIA) <yejin0621@tukorea.ac.kr>
2026-01-30 15:29:15 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.66 ms, Memory: 9.59 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,25 +1,20 @@
-from itertools import combinations  
+from itertools import combinations
 from collections import Counter
 
-#모든 경우의 수를 만들고 -> 하나만 뽑는다
 def solution(orders,course):
     answer = []
-    #course중 최대값 하나씩 찾아야하니까 필요
     for c in course:
         temp = []
         for order in orders:
-            #정렬 -> 조합으로 모든 경우의 수 찾기
             temp += combinations(sorted(order),c)
-            
-        counter = Counter(temp)
         
+        counter = Counter(temp)
+    
         if counter:
-            #제일 많이 주문된 것 뭐지?
-            max_count = max(counter.values())
-            if max_count >= 2:
+            max_counter = max(counter.values())
+            if max_counter >= 2:
                 for menu,count in counter.items():
-                    #제일 많이 주문된 것들 정답에 넣기
-                    if count == max_count:
+                    if count == max_counter:
                         answer.append("".join(menu))
                 
     return sorted(answer)
